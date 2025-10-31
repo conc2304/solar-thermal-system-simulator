65e70337960c650517f3e4382c81f3c979e67986
Jose Conchello <conc2304@users.noreply.github.com>
2025-10-31 13:00:08 -0700

    fix storybook parsing error

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,11 +8,16 @@ const config: StorybookConfig = {
   "addons": [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-onboarding",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
   ],
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
-  }
+  },
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
 };
 export default config;
